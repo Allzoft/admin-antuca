3eb86931dacce8f3dc7cda4d6db060c593d23455
Zolouz966 <zrodrigo96@gmail.com>
2023-12-18 10:28:21 -0400

    Add orders interface

--- src/app/interfaces/order.ts
+++ src/app/interfaces/order.ts
@@ -1,3 +1,9 @@
+import { Client } from './client';
+import { Customer } from './customer';
+import { Items } from './items';
+import { PaymentType } from './paymentType';
+import { State } from './state';
+
 export interface Order {
   id_order: number;
   customerIdCustomer: number;
@@ -7,4 +13,27 @@ export interface Order {
   total_amount: number;
   paymentTypeIdPaymentType: number;
   notes: string;
+  items?: createItems[];
+  created_at?: Date;
+  updated_at?: Date;
+  orderItems?: OrderItems[];
+  client: Client;
+  customer: Customer;
+  state: State;
+  paymentType: PaymentType;
+}
+
+export interface OrderItems {
+  id_order_item: number;
+  orderIdOrder: number;
+  itemIdItem: number;
+  quantity: number;
+  subtotal: number;
+  item: Items;
+}
+
+export interface createItems {
+  itemIdItem: number;
+  quantity: number;
+  subtotal: number;
 }
