7ad995ff863154c00ea5cd0cdbd297204759d488
Zolouz966 <zrodrigo96@gmail.com>
2023-12-17 23:43:36 -0400

    End states component

--- src/app/dashboard/pages/states/states.component.ts
+++ src/app/dashboard/pages/states/states.component.ts
@@ -1,12 +1,149 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { State } from '@interfaces/state';
+import { StatesService } from '@services/states.service';
+import { TitleComponent } from '@shared/title/title.component';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { SidebarModule } from 'primeng/sidebar';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-states',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
+    TableModule,
+    CardModule,
+    TitleComponent,
+    ButtonModule,
+    InputTextModule,
+    SidebarModule,
+    DropdownModule,
+    ToastModule,
+    ConfirmDialogModule,
   ],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './states.component.html',
 })
-export default class StatesComponent { }
+export default class StatesComponent {
+  private confirmationService = inject(ConfirmationService);
+  public statesService = inject(StatesService);
+  public messageService = inject(MessageService);
+  public states = this.statesService.states;
+  public filteredStates = [...this.statesService.states()];
+  public selectedState: State = {
+    id_state: 0,
+    name: '',
+    color: '',
+    priority: 0,
+    type: '',
+  };
+
+  public inputsDirt = {
+    name: false,
+    color: false,
+    priority: false,
+    type: false,
+  };
+
+  public stateOptions: string[] = ['Órdenes'];
+
+  public showState: boolean = false;
+
+  public refreshData(): void {
+    this.statesService.getAllStates();
+  }
+
+  public createState() {
+    this.selectedState = {
+      id_state: 0,
+      name: '',
+      color: '',
+      priority: 0,
+      type: '',
+    };
+    this.showState = true;
+  }
+
+  public editState(state: State) {
+    this.selectedState = state;
+    this.showState = true;
+  }
+
+  public deleteState(state: State) {
+    this.confirmationService.confirm({
+      message: 'Esta seguro de eliminar el estdo' + state.name,
+      acceptLabel: 'Si',
+      acceptButtonStyleClass: 'p-button-rounded p-button-success w-7rem',
+      rejectLabel: 'No',
+      rejectButtonStyleClass: 'p-button-rounded p-button-warning w-7rem',
+      header: 'Confirmación',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.statesService.deleteStates(state.id_state).subscribe((_) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Eliminación exitosa',
+            detail: `${state.name} eliminado exitosamente`,
+          });
+        });
+      },
+    });
+  }
+
+  public async saveState() {
+    if (!(await this.passStateForm())) return;
+
+    const newState: Partial<State> = {
+      name: this.selectedState.name,
+      color: this.selectedState.color,
+      priority: this.selectedState.priority,
+      type: this.selectedState.type,
+    };
+    if (this.selectedState.id_state === 0) {
+      this.statesService.postStates(newState).subscribe((resState) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Creación exitosa',
+          detail: `El estado ${resState.name} se creo exitosamente`,
+        });
+        this.showState = false;
+      });
+    } else {
+      this.statesService
+        .updateState(this.selectedState.id_state, newState)
+        .subscribe((resState) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Creación exitosa',
+            detail: `El estado ${resState.name} se actualizo exitosamente`,
+          });
+          this.showState = false;
+        });
+    }
+  }
+
+  public passStateForm(): Promise<boolean> {
+    if (!this.selectedState.name) {
+      this.inputsDirt.name = true;
+      return Promise.resolve(false);
+    }
+    if (!this.selectedState.type) {
+      this.inputsDirt.type = true;
+      return Promise.resolve(false);
+    }
+    if (!this.selectedState.color) {
+      this.inputsDirt.color = true;
+      return Promise.resolve(false);
+    }
+    return Promise.resolve(true);
+  }
+}
--- src/app/interfaces/state.ts
+++ src/app/interfaces/state.ts
@@ -2,7 +2,7 @@ export interface State {
   id_state: number;
   name: string;
   type: string;
-  priority: string;
+  priority: number;
   color: string;
   status?: number;
   created_at?: Date;
