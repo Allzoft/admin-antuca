089bb116a2cffb7d7b9291355b086ef9f9954073
Rodrigo Zelaya <zrodrigo96@gmail.com>
2024-11-10 16:13:47 -0400

    we add layput

---
+++ src/app/dashboard/layout.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LayoutService } from '@services/layout.service';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
+import { TopbarComponent } from '@shared/topbar/topbar.component';
+import UserComponent from './pages/user/user.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { CustomersService } from '@services/customers.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SidemenuComponent,
+    TopbarComponent,
+    RouterOutlet,
+    UserComponent,
+    SidebarModule,
+  ],
+  templateUrl: './layout.component.html',
+  styles: `
+  .w-0 {
+    width: 0%;
+  }
+  `
+})
+export default class DashboardComponent {
+  public layoutService = inject(LayoutService);
+  public usersService = inject(CustomersService)
+  public sidebarVisible: boolean = true
+}
