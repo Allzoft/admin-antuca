cfc05cfb9a0d537ef65757a459a64491aee618cc
Rodrigo Zelaya <120606993+Zoloz966@users.noreply.github.com>
2025-02-06 00:55:52 -0400

    feature(restaurants): start with restaurants module

---
+++ src/app/dashboard/pages/restaurants-list/restaurants-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurants-list.component.html',
+})
+export default class RestaurantsListComponent implements OnInit {
+
+  ngOnInit(): void { }
+
+}
